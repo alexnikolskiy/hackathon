9b91f93c75bc01dda1c2e2044df1233ad06347b2
Alexander Nikolskiy <alexxxnikolskiy@gmail.com>
2019-07-30 17:51:46 +0300

    Setup remote server settings

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <Router basename="/hackathon/">
           <Hero size="fullheight">
             <div className="wallpaper"></div>
 
